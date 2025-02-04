0b786f9807b906b1a9e1c256fd25a41057f97824
SatyaAkhilesh <pulavarthi.sa@northeastern.edu>
2025-02-04 18:49:53 -0500

    Update BullMarketStrategy.java

--- src/main/java/edu/neu/csye7374/BullMarketStrategy.java
+++ src/main/java/edu/neu/csye7374/BullMarketStrategy.java
@@ -3,13 +3,12 @@ package edu.neu.csye7374;
 public class BullMarketStrategy implements StockStrategy {
     @Override
     public void adjustPrice(StockAPI stock) {
-        double adjustmentFactor = Math.max(stock.getMetric() * 0.6, 5); // Ensure a meaningful change
+        double adjustmentFactor = Math.max(stock.getMetric() * 0.6, 5); //
         
-        // Check last bid: If negative, decrease price instead
+        
         if (((StockB) stock).getLastBid() < 0) {
-            stock.setPrice(stock.getPrice() - adjustmentFactor); // Decrease price for sell orders
+            stock.setPrice(stock.getPrice() - adjustmentFactor); 
         } else {
-            stock.setPrice(stock.getPrice() + adjustmentFactor); // Increase price for buy orders
-        }
+            stock.setPrice(stock.getPrice() + adjustmentFactor); 
     }
 }
